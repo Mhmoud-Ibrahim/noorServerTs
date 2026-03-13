af3156c3875727721c2a4c44e6dc4d81529ee7b1
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-03-14 02:48:54 +0400

    fff

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:3000",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, 
   allowedHeaders: ['Content-Type', 'Authorization']
