18e7d69fcc1e85ea1a50d53554f849bbdb699aef
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-03-18 19:39:11 +0400

    d

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.use(cors({
 }));
 dbConnections();
 app.use(cookieParser());
-app.use(express.json());
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
