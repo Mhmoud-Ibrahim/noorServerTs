8a9823c1c462b04ee77bd54cc8885cbe978814f7
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-03-21 01:11:38 +0400

    dfdf

--- src/server.ts
+++ src/server.ts
@@ -30,8 +30,8 @@ dbConnections();
 app.use(cookieParser());
 
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ limit: '15mb', extended: true }));
 
 app.use('/uploads', express.static('uploads'));
 app.use('/auth', authRouter);
