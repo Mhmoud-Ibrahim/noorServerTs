2e3e367a4dc7c75ca9a37608d9ea86be890565b4
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-04-14 00:24:14 +0400

    df

--- database/models/user.model.ts
+++ database/models/user.model.ts
@@ -14,7 +14,7 @@ const UserSchema = new mongoose.Schema<IUser>({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    userImage: { type: String, default: null },
+    userImage: { type: String },
       googleId: {
         type: String,
         required: false
