6a32d45ed9ba385f60a91c2d877cbcd5e28b12e5
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-04-17 17:32:40 +0400

    dfdfd

--- src/modules/auth/passport.ts
+++ src/modules/auth/passport.ts
@@ -13,7 +13,7 @@ import { Strategy as GoogleStrategy, type Profile, type VerifyCallback } from 'p
 passport.use(new GoogleStrategy({
     clientID: process.env.clientID!,
     clientSecret: process.env.clientSecret!,
-    callbackURL: process.env.callbackURL!,
+    callbackURL: "https://noor-server-ts.vercel.app/auth/google/callback",
   },
   async (_accessToken: string, _refreshToken: string, profile: Profile, done: VerifyCallback) => {
     try {
