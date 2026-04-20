7914d834d3ca7d286ef6e09e29c9bed7e4d68cec
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-04-21 00:08:17 +0400

    dfdf

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,13 +1,20 @@
 import nodemailer from 'nodemailer';
 
 export const sendEmail = async (options: any) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
+const transporter = nodemailer.createTransport({
+    host: "://gmail.com",
+    port: 465,
+    secure: true, // استخدام منفذ محمي 465
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-  });
+    // إضافة هذا الجزء لضمان عدم رفض الاتصال من السيرفرات الخارجية
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
 
   const mailOptions = {
     from: `Noor Store <${process.env.EMAIL_USER}>`,
