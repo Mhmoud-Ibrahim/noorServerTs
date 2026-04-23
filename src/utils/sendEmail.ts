80e076f42ee182fa7b0b622a833d1111ba613abe
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-04-23 23:36:36 +0400

    saeed

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -5,13 +5,13 @@ import { emailTemplate } from './emailtemplate.js';
 export const sendEmail = async (options: any) => {
 const transporter = nodemailer.createTransport({
     service:"gmail",
-
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
    
     tls: {
+        rejectUnauthorized: false
     }
 });
 
