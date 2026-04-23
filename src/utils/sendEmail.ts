fcce505fdc163ed06153f4b76ea51d827e58c3f6
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-04-23 22:34:08 +0400

    df

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer';
+import { emailTemlate } from './emailtemplate.js';
 
 export const sendEmail = async (options: any) => {
 const transporter = nodemailer.createTransport({
@@ -8,7 +9,7 @@ const transporter = nodemailer.createTransport({
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    // إضافة هذا الجزء لضمان عدم رفض الاتصال من السيرفرات الخارجية
+   
     tls: {
         rejectUnauthorized: false
     }
@@ -17,7 +18,8 @@ const mailOptions = await transporter.sendMail({
    from: `Noor Store <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
-    text: options.message,
+    // text: options.message,
+    html:emailTemlate()
 })
 
  
