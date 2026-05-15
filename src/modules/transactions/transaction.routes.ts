04b3b70ef7feaedbe8f7b893fae6d6132fc33997
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-05-15 16:29:18 +0400

    df

--- src/modules/transactions/transaction.routes.ts
+++ src/modules/transactions/transaction.routes.ts
@@ -4,8 +4,6 @@ import { authenticate, allowedTo } from "../../middleware/authintecate.js";
 
 const transactionRouter = Router();
 
-// مسار تسجيل المعاملات والمبيعات اليومية الكاش
-// محمي تماماً: يتأكد أولاً من التوكن (authenticate) ثم يفحص الصلاحية (allowedTo)
 transactionRouter.post(
   "/transactions", 
   authenticate, 
